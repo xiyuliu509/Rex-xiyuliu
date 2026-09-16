58954f88a80b7b0ae8211d7f9a45f7a06ebc7cbf
xiyuliu <118703788+xiyuliu509@users.noreply.github.com>
2026-09-16 11:13:37 +0800

    Add files via upload

---
+++ RexCollection.js
@@ -0,0 +1,52 @@
+{
+  "title": "Rex-xiyuliu",
+  "description": "For personal use only",
+  "icon": "https://raw.githubusercontent.com/MakkaPakka518/FW/refs/heads/main/widgets/tubiao/Rex-Makka.JPEG",
+  "widgets": [
+    {
+      "id": "guduo_hot_list_makka", 
+      "title": "骨朵热度指数榜", 
+      "description": "每日更新的CN剧集、动漫、综艺、电影全网热度排行", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.0.1", 
+      "author": "xiyuliu", 
+      "url": "https://rex.fwd.ccwu.cc/js/gdrdbRex.js"
+    },
+    { 
+      "id": "western_trends_hub_temp", 
+      "title": "欧美风向标|口碑与热度", 
+      "description": "聚合烂番茄(口碑)与流媒体平台(热度)", 
+      "requiredVersion": "0.0.1", 
+      "version": "1.1.5", 
+      "author": "xiyuliu", 
+      "url": "https://rex.fwd.ccwu.cc/js/westRex.js" 
+    },
+    {
+      "id": "anime_omni_fix_rex",
+      "title": "二次元全境聚合",
+      "description": "一站式聚合多平台动漫榜单。",
+      "requiredVersion": "0.0.1",
+      "version": "2.5.0",
+      "author": "xiyuliu",
+      "url": "https://rex.fwd.ccwu.cc/js/anime.js"
+    },
+    {
+      "id": "trakt_personal_key",
+      "title": "Trak 追剧日历 免key版",
+      "description": "内置 Key 版：只需填写用户名即可使用。显示追剧日历、待看、收藏及历史记录",
+      "requiredVersion": "0.0.1",
+      "version": "1.1.9",
+      "author": "xiyuliu",
+      "url": "https://rex.fwd.ccwu.cc/js/trakt.js"
+    },
+    {
+      "id": "zhuijurili",
+      "title": "追剧日历",
+      "description": "追剧日历(今/明日播出、周历、各项榜单、今日推荐)",
+      "requiredVersion": "0.0.1",
+      "version": "1.0.4",
+      "author": "xiyuliur",
+      "url": "https://rex.fwd.ccwu.cc/js/zjrllogvar.js"
+    }
+  ]
+}
